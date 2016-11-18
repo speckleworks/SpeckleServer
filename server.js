9abf7afdfdd07abadb22f1096fd93898ead2acf1
dimitrie <Bender@eduroam-int-dhcp-97-48-85.ucl.ac.uk>
2016-11-18 16:59:01 +0000

    Basic Express Server & WSS

--- server.js
+++ server.js
@@ -1,18 +1,55 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
- 
-wss.on('connection', function connection(ws) {
-  console.log('got connection')
-  
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    setTimeout( function() {    
-        ws.send('hai', function ack(error) {
-            console.log(error)
-        })
-    }, 1000)
-    });
+var express             = require('express')
+var compression         = require('compression')
+var cors                = require('cors')
+var cookieParser        = require('cookie-parser')
+var bodyParser          = require('body-parser')
+var passport            = require('passport')
+var path                = require('path')
+var chalk               = require('chalk')
+var winston             = require('winston')
+var expressWinston      = require('express-winston')
 
-  ws.send('something');
-  ws.send('something else')
-});
+
+var mongoose            = require('mongoose')
+var deets               = require('./.secrets/database')
+mongoose.connect( deets.url , ( err ) => {
+  if( err ) throw err
+  else winston.info('connected to mongoose at ' + deets.url )
+})
+
+var app = express() 
+app.use( cors() )
+app.use( compression() )
+
+app.use( expressWinston.logger( {
+  transports: [ new winston.transports.Console( { json: false, colorize: true } ) ],
+  meta: false,
+  msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}}'
+} ))
+
+app.use( cookieParser() )
+app.use( bodyParser.json( { limit: '50mb' } ) )
+
+app.use( '/admin', express.static( './frontend/admin' ) )
+
+var http = require('http')
+var server = http.createServer( app )
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer( { server: server } )
+
+wss.on('connection', ( ws ) => {
+  ws.on( 'message', message => {
+    console.log(message)
+  } )
+})
+
+app.get('/', function(req, res) {
+  res.send('Hello there. Move along now.')
+})
+
+var PORT = 8080
+server.listen( PORT, () => {
+  winston.info( chalk.red.underline( 'Starting up @ ' + PORT ) )
+})
+
+
