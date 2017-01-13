af780fbec0377a647bf70b22ea4aa27cbbfbd15b
dimitrie <Bender@Benders-MacBook-Pro.local>
2017-01-13 17:25:02 +0000

    Require the right UWS dependency

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use( '/admin', express.static( './frontend/admin' ) )
 
 var http = require('http')
 var server = http.createServer( app )
-var WebSocketServer = require('ws').Server
+var WebSocketServer = require('uws').Server
 
 var wss = new WebSocketServer( { 
   server: server, 
