2ea6f7cf4bc601951d94ea91bd66634d2e61f02c
dimitrie <Bender@Benders-MacBook-Pro.local>
2017-01-28 16:11:13 +0000

    Changing model historyInstance
    Added objectProperties list for user props

--- models/HistoryInstance.js
+++ models/HistoryInstance.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose')
 
 var historyInstanceSchema = mongoose.Schema( {
+  // name of history instance
   name: { type: String },
-  structure: { type: Array, default: [] }, 
-  layers: { type: Array, default: [] },
+  // general properties can go in here
   properties: { type: Array, default: [] },
-  // this is (might be) an antipattern
+  // layer list
+  layers: { type: Array, default: [] },
+  // object list, mixed embedded values and refs
   objects: { type: Array, default: [] },
+  // user properties list for objects (if any)
+  objectProperties: { type: Array, default: [] }
 }, { timestamps: true } )
 
 module.exports = mongoose.model('HistoryInstance', historyInstanceSchema)
