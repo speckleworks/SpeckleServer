632889ee61e751de496c7f820b46ef1cf478e39e
Dimitrie <didimitrie@gmail.com>
2017-03-27 17:05:50 +0100

    Returning server description on `/`route

--- .config/serverDescription.js
+++ .config/serverDescription.js
@@ -1,10 +1,5 @@
 module.exports = {
-  serverName: 'Speckle Test Deployment',
+  serverName: 'Speckle OSX Localhost',
   restApi: 'http://10.211.55.2:8080/api/v1',
-  ws: 'ws://10.211.55.2:8080',
-  resources: {
-    accounts: 'http://localhost:8080/api/v1/accounts',
-    streams: 'http://localhost:8080/api/v1/streams',
-    geometry: 'http://localhost:8080/api/v1/geometry'
-  }
+  ws: 'ws://10.211.55.2:8080'
 }
