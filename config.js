a53a9b92fe21671ab8c2ae844aa15d0508ac8738
John Egan <john@jenca.io>
2017-04-04 17:28:44 +0100

    update readme to docker deploy, make possible to set server env vars in docker-compose.yml

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ module.exports = {
     port: process.env.PORT || 8080
   },
   speckle: {
-    serverName: process.env.SPECKLE_NAME || 'Speckle OSX Localhost',
-    restApi: process.env.SPECKLE_URL || 'http://127.0.0.1:8080/api/v1',
-    ws: process.env.SPECKLE_SOCKET || 'ws://127.0.0.1:8080'
+    serverName: process.env.SPECKLE_NAME || 'Speckle OSX Localhost',  // Not really important
+    restApi: process.env.SPECKLE_URL || 'http://127.0.0.1:8080/api/v1', // Replace 'http://...` with the IP address your server is running on.
+    ws: process.env.SPECKLE_SOCKET || 'ws://127.0.0.1:8080' // Same!
   },
   mongo: {
     url: `mongodb://${user}:${pw}@${host}:${port}/${name}?authSource=${auth}`
