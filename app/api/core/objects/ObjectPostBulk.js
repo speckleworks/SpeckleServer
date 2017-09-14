fe06e6849b14f580d73446e39fbab72ceeb21c84
Dimitrie Stefanescu <didimitrie@gmail.com>
2017-09-14 16:26:14 +0200

    Fixed possible future bug re placeholder objects

--- app/api/core/objects/ObjectPostBulk.js
+++ app/api/core/objects/ObjectPostBulk.js
@@ -6,15 +6,14 @@ const _ = require( 'lodash' )
 const SpeckleObject = require( '../../../../models/SpeckleObject' )
 
 // (1) Update all the objects that have an _id (replaces them completely)
-// (2) Check in the db for existing objects by hash & map their _id to the 
+// (2) Check in the db for existing objects by hash & map their _id to the obj list
 // (3) Insert all the new objects
 module.exports = ( req, res ) => {
   if ( !req.body.objects ) {
     res.status( 400 )
     return res.send( { success: false, message: 'Malformed request.' } )
   }
-  
-  Promise.all( req.body.objects.reduce( ( arr, o ) => o._id !== undefined ? [ SpeckleObject.update( { _id: o._id }, o ), ...arr ] : arr, [ ] ) )
+  Promise.all( req.body.objects.reduce( ( arr, o ) => ( o._id !== undefined && o.type !== 'Placeholder' ? [ SpeckleObject.update( { _id: o._id }, o ), ...arr ] : arr, [ ] ) )
     .then( ( ) => SpeckleObject.find( { hash: { $in: req.body.objects.reduce( ( arr, o ) => o._id === undefined ? [ o.hash, ...arr ] : arr, [ ] ) } }, '_id hash' ) )
     .then( results => {
       results.forEach( o => req.body.objects.filter( oo => oo.hash == o.hash ).forEach( oo => oo._id = o._id.toString( ) ) )
