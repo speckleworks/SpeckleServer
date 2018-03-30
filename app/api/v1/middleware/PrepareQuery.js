62351cc826dc861cb1ec1f8abc27bf060aed2e4c
Dimitrie Stefanescu <didimitrie@gmail.com>
2018-03-30 15:11:41 +0100

    fixes #66

--- app/api/v1/middleware/PrepareQuery.js
+++ app/api/v1/middleware/PrepareQuery.js
@@ -1,4 +1,3 @@
-'use strict'
 const winston = require( 'winston' )
 const chalk = require( 'chalk' )
 const q2m = require( 'query-to-mongo' )
@@ -6,9 +5,25 @@ const q2m = require( 'query-to-mongo' )
 /*
 Ensures a query always comes along with the needed fields for permission checks
  */
+
+let enforcedFields = [ 'type', 'private', 'canRead', 'canWrite', 'owner' ]
+
 module.exports = ( q ) => {
   let query = q2m( q )
-  if ( query.options.fields )
-    query.options.fields.type = query.options.fields.private = query.options.fields.canRead = query.options.fields.canWrite = query.options.fields.owner = 1
+  let isHacker = false;
+  if ( query.options.fields ) {
+    if ( q.omit === undefined ) {
+      enforcedFields.forEach( field => query.options.fields[ field ] = 1 )
+      return query
+    }
+    enforcedFields.forEach( field => {
+      if ( query.options.fields[ field ] != undefined ) {
+        isHacker = true
+        delete query.options.fields[ field ]
+      }
+    } )
+    winston.debug( chalk.white.bgMagenta( 'Someone is a hacker' ) )
+    return query
+  }
   return query
 }
