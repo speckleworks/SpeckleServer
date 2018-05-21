df10599f5b9ef28eb1232ea18ae96862ccec7bc5
Dimitrie Stefanescu <didimitrie@gmail.com>
2018-05-21 19:34:59 +0100

    fixes #65

--- models/User.js
+++ models/User.js
@@ -18,6 +18,8 @@ var userSchema = mongoose.Schema({
 userSchema.pre( 'save', function( next ) {
   var user = this
   if( this.isModified( 'password' ) || this.isNew ) {
+    if( user.password.length < 8 )
+      return next( new Error( 'Password too short.' ) )
     bcrypt.genSalt( 10, function ( err, salt ) {
       if( err ) return next( err )
       bcrypt.hash( user.password, salt, null, function ( err, hash ) {
