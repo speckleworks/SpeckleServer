9ffa0b75674c6b5325e3aa5dd1a5dc79292b21f5
Logan Glasson <me@loganglasson.com>
2018-07-16 23:05:17 +1200

    Add ESLint and a lenient (no changes to existing code required) ESLint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2015,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-unused-vars": "off",
+    "no-console": "off",
+    "no-dupe-keys": "off"
+  }
+};
