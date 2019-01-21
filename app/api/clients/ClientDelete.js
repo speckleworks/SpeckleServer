d1b98550c88d23b62b34e6f40997e90ef4e66597
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-01-21 15:51:56 +0000

    moves resource permission checks on client edit routes from the client to the stream

--- app/api/clients/ClientDelete.js
+++ app/api/clients/ClientDelete.js
@@ -2,6 +2,8 @@
 const winston = require( '../../../config/logger' )
 
 const Client = require( '../../../models/UserAppClient' )
+const DataStream = require( '../../../models/DataStream' )
+
 const PermissionCheck = require( '../middleware/PermissionCheck' )
 
 module.exports = ( req, res ) => {
@@ -9,18 +11,22 @@ module.exports = ( req, res ) => {
     res.status( 400 )
     return res.send( { success: false, message: 'No stream id provided.' } )
   }
-
+  let client = null
   Client.findOne( { _id: req.params.clientId } )
-    .then( client => PermissionCheck( req.user, 'delete', client ) )
-    .then( client => {
-      return client.remove()
+    .then( result => {
+      client = result
+      return DataStream.findOne( { streamId: client.streamId }, 'canRead canWrite owner _id' )
     } )
-    .then( () => {
+    .then( stream => PermissionCheck( req.user, 'write', stream ) )
+    .then( ( ) => {
+      return client.remove( )
+    } )
+    .then( ( ) => {
       return res.send( { success: true, message: 'Client was deleted! Bye bye data.' } )
     } )
     .catch( err => {
       winston.error( JSON.stringify( err ) )
       res.status( err.message === 'Unauthorized. Please log in.' ? 401 : 404 )
-      res.send( { success: false, message: err.toString() } )
+      res.send( { success: false, message: err.toString( ) } )
     } )
 }
--- app/api/clients/ClientGet.js
+++ app/api/clients/ClientGet.js
@@ -1,6 +1,8 @@
 const winston = require( '../../../config/logger' )
 
 const Client = require( '../../../models/UserAppClient' )
+const DataStream = require( '../../../models/DataStream' )
+
 const PermissionCheck = require( '../middleware/PermissionCheck' )
 
 module.exports = ( req, res ) => {
@@ -8,11 +10,16 @@ module.exports = ( req, res ) => {
     res.status( 400 )
     return res.send( { success: false, message: 'Malformed request.' } )
   }
+  let client = null
   Client.findOne( { _id: req.params.clientId } ).populate( 'owner', 'name surname email company' )
+    .then( result => {
+      client = result
+      return DataStream.findOne( { streamId: client.streamId }, 'canRead canWrite owner _id' )
+    } )
     .then( result => PermissionCheck( req.user, 'read', result ) )
     .then( result => {
       if ( !result ) throw new Error( 'No client found.' )
-      res.send( { success: true, message: 'Client found.', resource: result } )
+      res.send( { success: true, message: 'Client found.', resource: client } )
     } )
     .catch( err => {
       winston.error( JSON.stringify( err ) )
