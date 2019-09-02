2dac8590e5b60cea695d83ba9f1c306cb05c7405
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-09-02 18:37:35 +0100

    feat(telemetry): scaffolding extracting the server version from git repo status

--- telemetry.js
+++ telemetry.js
@@ -1,8 +1,22 @@
 const countly = require( 'countly-sdk-nodejs' )
 const machineIdSync = require( 'node-machine-id' ).machineIdSync
 
+const { exec } = require( 'child_process' )
+
+// async function ls() {
+
 module.exports = ( ) => {
   let myMachineId = machineIdSync( )
+  let tagVersion = 'unknown'
+
+  try {
+    exec( 'git describe --tags', ( err, stdout, stderr ) => {
+      tagVersion = stdout
+    } )
+  } catch {
+
+  }
+
   countly.init( {
     app_key: '6b79ee267ff23c4b99108591c5b33f0ba8ed5e4b',
     url: 'https://telemetry.speckle.works',
