768d1c49b2be42d15c6df89a5501fcab37ff3b27
AntoineDao <antoinedao1@gmail.com>
2019-10-28 22:31:47 +1000

    test(setup): setup basic testing infrastructure and refractor where needed
    added some basic testing utility code, mongodb setup and teardown, testing .env file and mocha +
    chai imports

---
+++ test/mongoose-setup-teardown.js
@@ -0,0 +1,19 @@
+const mongoose = require( 'mongoose' );
+const { MongoMemoryServer } = require( 'mongodb-memory-server' );
+
+let mongoServer;
+
+before( async () => {
+  mongoose.Promise = global.Promise
+  mongoose.set( 'useCreateIndex', true );
+  const mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getConnectionString();
+  await mongoose.connect( mongoUri, {
+    useNewUrlParser: true,
+  } );
+} )
+
+after( async () => {
+  await mongoose.disconnect()
+  // await mongoServer.stop()
+} )
