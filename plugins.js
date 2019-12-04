1a32e3737f5ba13340979af794e32ba2ffdf671b
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-12-04 15:04:16 +0000

    style(linting): fixed

--- plugins.js
+++ plugins.js
@@ -1,7 +1,7 @@
 const fs = require( 'fs' )
 const path = require( 'path' )
 const winston = require( './config/logger' )
-const URL = require( 'url').URL
+const URL = require( 'url' ).URL
 
 const isDirectory = source => fs.lstatSync( source ).isDirectory( )
 const getDirectories = source => fs.readdirSync( source ).map( name => path.join( source, name ) ).filter( isDirectory )
