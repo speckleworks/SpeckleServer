f8419a2bb3ef90f9f553491a0cde45be49b66354
Alan Rynne <alan@rynne.es>
2020-01-22 13:48:51 +0100

    Added dist from speckleadmin

---
+++ plugins/MPDA-SpeckleAdmin/dist/precache-manifest.897218b623207da77b582becd675c77c.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e887165427b67fa0065",
+    "url": "css/app.acad438d.css"
+  },
+  {
+    "revision": "3e887165427b67fa0065",
+    "url": "js/app.36f58e49.js"
+  },
+  {
+    "revision": "1bcfa3f23a4a3f4622e9",
+    "url": "css/chunk-0083a15b.b2d96ffe.css"
+  },
+  {
+    "revision": "1bcfa3f23a4a3f4622e9",
+    "url": "js/chunk-0083a15b.aa8585a5.js"
+  },
+  {
+    "revision": "b59f843cd81833df926a",
+    "url": "css/chunk-080abe1e.c22e1483.css"
+  },
+  {
+    "revision": "b59f843cd81833df926a",
+    "url": "js/chunk-080abe1e.c3689356.js"
+  },
+  {
+    "revision": "be785cafb63245203786",
+    "url": "css/chunk-0a9372bf.014b52ed.css"
+  },
+  {
+    "revision": "be785cafb63245203786",
+    "url": "js/chunk-0a9372bf.ab5d6fa7.js"
+  },
+  {
+    "revision": "3627ff7464edadd969c3",
+    "url": "css/chunk-298df38e.3ef275e0.css"
+  },
+  {
+    "revision": "3627ff7464edadd969c3",
+    "url": "js/chunk-298df38e.fa617d52.js"
+  },
+  {
+    "revision": "d65d4fa8d0bab5fe8c80",
+    "url": "js/chunk-2d0abfdc.e3de337b.js"
+  },
+  {
+    "revision": "b6d1eee46cea64261f58",
+    "url": "js/chunk-2d0af48a.57711a66.js"
+  },
+  {
+    "revision": "d6364a95c8dc2144bac2",
+    "url": "js/chunk-2d0b3630.dfd5e278.js"
+  },
+  {
+    "revision": "92e0a23d6779afd8a81e",
+    "url": "js/chunk-2d0b9d35.7b85c2f4.js"
+  },
+  {
+    "revision": "1640bc432964417ae2ec",
+    "url": "js/chunk-2d0cb6c3.163e2d4b.js"
+  },
+  {
+    "revision": "faf5d6cbf24fcb6a45e2",
+    "url": "js/chunk-2d0d7de8.6d5249f8.js"
+  },
+  {
+    "revision": "94ff76b638aaa179176e",
+    "url": "js/chunk-2d0d9f44.1e1b50e4.js"
+  },
+  {
+    "revision": "3e1dcb120ae8188783a5",
+    "url": "js/chunk-2d0db2c8.54e5899a.js"
+  },
+  {
+    "revision": "c12ee87dca34c1786340",
+    "url": "js/chunk-2d2138f6.d824e297.js"
+  },
+  {
+    "revision": "72ab8af2f90396d18e1b",
+    "url": "js/chunk-2d217357.8f2a9bef.js"
+  },
+  {
+    "revision": "0aaf5099b40156ae0d5a",
+    "url": "js/chunk-2d21ed68.a9718953.js"
+  },
+  {
+    "revision": "1ca9aaa3317ff087afe7",
+    "url": "js/chunk-2d22d1db.a18fbe82.js"
+  },
+  {
+    "revision": "21340e51f16eab21c997",
+    "url": "css/chunk-35bc4d64.d301baa8.css"
+  },
+  {
+    "revision": "21340e51f16eab21c997",
+    "url": "js/chunk-35bc4d64.badf83ce.js"
+  },
+  {
+    "revision": "7d194e10fb8b404770fc",
+    "url": "css/chunk-43e930b8.4e93b041.css"
+  },
+  {
+    "revision": "7d194e10fb8b404770fc",
+    "url": "js/chunk-43e930b8.d24540e0.js"
+  },
+  {
+    "revision": "cc51ef8da6fdff67cbd4",
+    "url": "css/chunk-4ab64705.e0850488.css"
+  },
+  {
+    "revision": "cc51ef8da6fdff67cbd4",
+    "url": "js/chunk-4ab64705.8f19b6e8.js"
+  },
+  {
+    "revision": "32d6dc5f9d9493cd9f36",
+    "url": "js/chunk-6e6c150c.6427a6d8.js"
+  },
+  {
+    "revision": "0488d41421a33afdbb93",
+    "url": "css/chunk-7abd522e.71cede48.css"
+  },
+  {
+    "revision": "0488d41421a33afdbb93",
+    "url": "js/chunk-7abd522e.ca5e6a11.js"
+  },
+  {
+    "revision": "38ca2bc8b96adc9b2ee3",
+    "url": "js/chunk-a45fa086.32159bb9.js"
+  },
+  {
+    "revision": "e51671b14fb972a198d8",
+    "url": "css/chunk-a5f28366.2380be23.css"
+  },
+  {
+    "revision": "e51671b14fb972a198d8",
+    "url": "js/chunk-a5f28366.f801b5b2.js"
+  },
+  {
+    "revision": "226dee34e15afec252b8",
+    "url": "js/chunk-bea16dec.4199e3a8.js"
+  },
+  {
+    "revision": "36086e2b14184dd165db",
+    "url": "js/chunk-c45573d2.a0caa85f.js"
+  },
+  {
+    "revision": "02d4cac71988b431b6bb",
+    "url": "css/chunk-f2159fa4.e463e98c.css"
+  },
+  {
+    "revision": "02d4cac71988b431b6bb",
+    "url": "js/chunk-f2159fa4.5ab0aec8.js"
+  },
+  {
+    "revision": "a5cbf96d4a68316b4f0d",
+    "url": "css/chunk-vendors.f3f52544.css"
+  },
+  {
+    "revision": "a5cbf96d4a68316b4f0d",
+    "url": "js/chunk-vendors.da68da72.js"
+  },
+  {
+    "revision": "d86ed65057cd5bd0b6b0474708985435",
+    "url": "index.html"
+  },
+  {
+    "revision": "63d1f3661678a1067db993b883ae0ee6",
+    "url": "msal.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
