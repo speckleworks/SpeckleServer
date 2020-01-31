c97dfa3e92ec1a465bace902e4f3a8a84e68c974
AntoineDao <antoinedao1@gmail.com>
2020-02-01 09:26:24 +1000

    test(websocket): add initial batch of websocket tests
    Had issues with set up and teardown for tests that are commented out. The tests pass but only if the
    test before it doesn't run... It's weird...

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 process.env.LOG_LEVEL = 'error';
@@ -5,18 +6,31 @@ process.env.LOG_LEVEL = 'error';
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const passport = require( 'passport' );
-const jwt = require( 'jsonwebtoken' )
+const jwt = require( 'jsonwebtoken' );
+const ws = require( 'ws' );
 
 const User = require( '../models/User' )
 const loadAPI = require( '../app/api' );
 const loadPassport = require( '../config/passport' )
 const loadEnv = require( '../config/loadEnv' );
 
+const sinon = require( 'sinon' );
+const redis = require( 'redis' )
+const mockRedis = require( "redis-mock" );
+
+// Setup redis mock before importing specklesockets
+const mock = sinon.createSandbox();
+const mockRedisClient = mockRedis.createClient()
+// mock.stub( redis, 'createClient' ).returns( mockRedisClient )
+mock.stub( redis, 'createClient' ).callsFake( mockRedis.createClient );
+
+const SpeckleSockets = require( '../app/ws/SpeckleSockets' )
+
 loadEnv()
 
 const REQ_SIZE = process.env.REQ_SIZE
 const SESSION_SECRET = process.env.SESSION_SECRET
-
+const PORT = process.env.PORT
 
 const newAPIServer = ( urlRoot, plugins ) => {
   const app = express()
@@ -30,6 +44,15 @@ const newAPIServer = ( urlRoot, plugins ) => {
   loadAPI( app, express, urlRoot, plugins )
 
   return app
+}
+
+const newWebsocketServer = ( ) => {
+
+  var wss = new ws.Server( { port: PORT } )
+
+  SpeckleSockets( wss )
+
+  return wss
 }
 
 const createTestUser = async ( email, role ) => {
@@ -47,7 +70,22 @@ const createTestUser = async ( email, role ) => {
   return testUser.save()
 }
 
+const asyncWs = async ( connectionString ) => {
+  const client = new ws( connectionString );
+
+  return new Promise( ( resolve, reject ) => {
+
+    setTimeout( () => {
+      return resolve( client )
+    }, 300 )
+
+  } )
+}
+
 module.exports = {
   newAPIServer,
-  createTestUser
+  createTestUser,
+  newWebsocketServer,
+  mockRedisClient,
+  asyncWs
 }
